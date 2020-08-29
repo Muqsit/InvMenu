83448739d16d330fe27efac0fef7a3cd994cf159
Muqsit <hackhack05@gmail.com>
2020-08-29 11:48:40 +0000

    Fix undefined offset crash when PlayerLoginEvent is cancelled

--- src/muqsit/invmenu/session/PlayerManager.php
+++ src/muqsit/invmenu/session/PlayerManager.php
@@ -33,8 +33,10 @@ final class PlayerManager{
 	}
 
 	public static function destroy(Player $player) : void{
-		self::$sessions[$player_id = $player->getId()]->finalize();
-		unset(self::$sessions[$player_id]);
+		if(isset(self::$sessions[$player_id = $player->getId()])){
+			self::$sessions[$player_id]->finalize();
+			unset(self::$sessions[$player_id]);
+		}
 	}
 
 	public static function get(Player $player) : ?PlayerSession{
