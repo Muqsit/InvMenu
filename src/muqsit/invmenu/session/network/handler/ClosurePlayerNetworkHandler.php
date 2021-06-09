a4553e7764ddad6335ad7000447dcafe06189793
Muqsit <hackhack05@gmail.com>
2021-06-09 03:54:59 +0000

    Remove license mention from code

--- src/muqsit/invmenu/session/network/handler/ClosurePlayerNetworkHandler.php
+++ src/muqsit/invmenu/session/network/handler/ClosurePlayerNetworkHandler.php
@@ -1,22 +1,5 @@
 <?php
 
-/*
- *  ___            __  __
- * |_ _|_ ____   _|  \/  | ___ _ __  _   _
- *  | || '_ \ \ / / |\/| |/ _ \ '_ \| | | |
- *  | || | | \ V /| |  | |  __/ | | | |_| |
- * |___|_| |_|\_/ |_|  |_|\___|_| |_|\__,_|
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * @author Muqsit
- * @link http://github.com/Muqsit
- *
-*/
-
 declare(strict_types=1);
 
 namespace muqsit\invmenu\session\network\handler;
