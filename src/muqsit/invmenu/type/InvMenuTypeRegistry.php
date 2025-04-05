f28377563f99427ecba4f6be916020520fcb5fe7
Muqsit <hackhack05@gmail.com>
2025-04-05 18:54:18 +0000

    Address #246

--- src/muqsit/invmenu/type/InvMenuTypeRegistry.php
+++ src/muqsit/invmenu/type/InvMenuTypeRegistry.php
@@ -27,7 +27,7 @@ final class InvMenuTypeRegistry{
 			->setBlock(VanillaBlocks::CHEST())
 			->setSize(54)
 			->setBlockActorId("Chest")
-			->setAnimationDuration(75)
+			->setAnimationDuration(1)
 		->build());
 
 		$this->register(InvMenuTypeIds::TYPE_HOPPER, InvMenuTypeBuilders::BLOCK_ACTOR_FIXED()
