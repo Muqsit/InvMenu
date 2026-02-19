ba123f12454a0c53851764b0edc15f4ceb2e452c
Remminiscent <remminiscentr@gmail.com>
2026-02-19 11:17:46 +0300

    Release v4.7.4

--- src/muqsit/invmenu/InvMenuEventHandler.php
+++ src/muqsit/invmenu/InvMenuEventHandler.php
@@ -10,6 +10,7 @@ use muqsit\invmenu\session\PlayerWindowDispatcher;
 use pocketmine\event\inventory\InventoryCloseEvent;
 use pocketmine\event\inventory\InventoryTransactionEvent;
 use pocketmine\event\Listener;
+use pocketmine\event\server\DataPacketDecodeEvent;
 use pocketmine\event\server\DataPacketReceiveEvent;
 use pocketmine\inventory\transaction\action\SlotChangeAction;
 use pocketmine\network\mcpe\protocol\ContainerClosePacket;
@@ -21,6 +22,22 @@ final class InvMenuEventHandler implements Listener{
 	public function __construct(
 		readonly private PlayerManager $player_manager
 	){}
+
+    /**
+     * @param DataPacketDecodeEvent $event
+     * @priority NORMAL
+     * @handleCancelled
+     */
+    public function onDataPacketDecode(DataPacketDecodeEvent $event) : void{
+        $packet_id = $event->getPacketId();
+        if(
+            $packet_id === NetworkStackLatencyPacket::NETWORK_ID ||
+            $packet_id === ContainerClosePacket::NETWORK_ID ||
+            $packet_id === PacketViolationWarningPacket::NETWORK_ID
+        ){
+            $event->uncancel();
+        }
+    }
 
 	/**
 	 * @param DataPacketReceiveEvent $event
