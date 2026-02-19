7563f6bc4ed9fbb58281cae3b196a6d92ff6f39a
Muqsit <hackhack05@gmail.com>
2026-02-19 08:22:43 +0000

    fix class-not-found: DataPacketDecodeEvent

--- src/muqsit/invmenu/InvMenuEventHandler.php
+++ src/muqsit/invmenu/InvMenuEventHandler.php
@@ -10,6 +10,7 @@ use muqsit\invmenu\session\PlayerWindowDispatcher;
 use pocketmine\event\inventory\InventoryCloseEvent;
 use pocketmine\event\inventory\InventoryTransactionEvent;
 use pocketmine\event\Listener;
+use pocketmine\event\server\DataPacketDecodeEvent;
 use pocketmine\event\server\DataPacketReceiveEvent;
 use pocketmine\inventory\transaction\action\SlotChangeAction;
 use pocketmine\network\mcpe\protocol\ContainerClosePacket;
